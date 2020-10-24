a89b25c20b2211249be01b2c20685096685e785e
jeremy935 <66013357+jeremy935@users.noreply.github.com>
2020-10-24 09:19:39 -0500

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -5,24 +5,34 @@ class HomePage extends StatelessWidget{
   
   final estiloTexto = new  TextStyle( fontSize: 25 );
 
-final conteo = 10;
+    final conteo = 10;
+
+
+
+
    
    @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+     Widget build(BuildContext context) {
+       return Scaffold(
+        appBar: AppBar(
         title: Text('Titulo'),
         centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
+        ),
+
+
+
+         body: Center(
+          child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+            children: <Widget>[
             Text('Numero de clicks:', style: estiloTexto ),
             Text('$conteo' , style: estiloTexto ),
           ],
         )
       ),
+
+
+      
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.add ),
         onPressed: (){
